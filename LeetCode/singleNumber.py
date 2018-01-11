5533bcb13609c2ed117041db5b4312b8c23b63b6
slimshady2378 <hulin237893@163.com>
2018-01-11 23:05:09 +0800

    add singleNumber Code for Python

---
+++ LeetCode/singleNumber.py
@@ -0,0 +1,23 @@
+
+def singleNumber(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    if not nums:
+        return 0
+    count = 0
+    n = len(nums)
+    nums.sort()
+    i = 0
+    if len(nums) == 1:
+        return nums[0]
+    while (count < n - 1):
+        if len(nums) == 1:
+            return nums[0]
+        if nums[0] != nums[1]:
+            return nums[i]
+        if nums[0] == nums[1]:
+            del nums[0]
+            del nums[0]
+        count += 1
